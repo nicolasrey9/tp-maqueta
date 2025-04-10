90eae50f81358245af7ea002fe72b2fce301eda9
nicolasrey9 <nirey@frba.utn.edu.ar>
2025-04-10 17:02:44 -0300

    logica de la fuenteDeDatos estatica para extraer hechos buscando similitudes en las columnas con los valores buscados, faltaria pulir casos de valores buscados que se representen en columnas multiples

--- src/main/java/org/example/FuenteDeDatosEstatica.java
+++ src/main/java/org/example/FuenteDeDatosEstatica.java
@@ -7,11 +7,21 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class FuenteDeDatosEstatica {
+
+  private static final List<String> titulos = Arrays.asList("event", "name", "titulo");
+  private static final List<String> descripciones = Arrays.asList("desc", "descripcion");
+  private static final List<String> categorias = Arrays.asList("tipo", "grupo", "categoria", "causa");
+  private static final List<String> latitudes = Arrays.asList("lat", "latitude", "latitud");
+  private static final List<String> longitudes = Arrays.asList("long", "lng", "lon", "logitude", "longitud");
+  private static final List<String> fechas = Arrays.asList("fecha", "date", "start");
+
+
+
+
   public static void extraerHechos(Path rutaDataset) {
     if(!esArchivoCSV(rutaDataset)) { throw new RuntimeException("Solo hay soporte para .csv actualmente"); }
 
@@ -21,12 +31,13 @@ public class FuenteDeDatosEstatica {
         String[] line;
         line = csvReader.readNext();
 
-        short posicionTitulo = obtenerPosicionTitulo(line);
-        short descripcion = obtenerPosiciondescripcion(line);
-        short categoria = obtenerPosicioncategoria(line);
-        short latitud = obtenerPosicionlatitud(line);
-        short longitud = obtenerPosicionlongitud(line);
-        short fecha = obtenerPosicionfecha(line);
+        short posicionTitulo = buscarColumna(line, true, titulos.toArray(new String[0]));
+        short descripcion = buscarColumna(line, true, descripciones.toArray(new String[0]));
+        short categoria = buscarColumna(line, true, categorias.toArray(new String[0]));
+        short latitud = buscarColumna(line, false,latitudes.toArray(new String[0]));
+        short longitud = buscarColumna(line, false,longitudes.toArray(new String[0]));
+        short fecha = buscarColumna(line, false, fechas.toArray(new String[0]));
+
 
         // iteraciones se usa solo para el test, BORRAR
         int iteraciones = 0;
@@ -47,26 +58,23 @@ public class FuenteDeDatosEstatica {
     return rutaArchivo.getFileName().toString().endsWith(".csv");
   }
 
-  private static short obtenerPosicionTitulo(String[] line) {
-  }
-
-  private static short obtenerPosicionfecha(String[] line) {
-  }
-
-  private static short obtenerPosicionlongitud(String[] line) {
-  }
-
-  private static short obtenerPosicionlatitud(String[] line) {
-  }
-
-  private static short obtenerPosicioncategoria(String[] line) {
-  }
-
-  private static short obtenerPosiciondescripcion(String[] line) {
-  }
 
 
-  private static short obtenerPosicionDe(String[] line, ) {
+
+  private static short buscarColumna(String[] encabezado, boolean modoFlexible, String... posiblesNombres) {
+    for (short i = 0; i < encabezado.length; i++) {
+      String nombreColumna = encabezado[i].toLowerCase().trim();
+      for (String posible : posiblesNombres) {
+        posible = posible.toLowerCase();
+        if (
+            nombreColumna.equals(posible) ||
+                (modoFlexible && nombreColumna.startsWith(posible))
+        ) {
+          return i;
+        }
+      }
+    }
+    return -1;  // manejo de errores
   }
 
 
